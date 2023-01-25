3975556b9f7769e21918861b59041edb495e13a6
“mahasedra” <mahasedra01@gmail.com>
2023-01-25 15:01:48 +0300

    feat: Validate the Information Received in the Subscription Form

--- app/Http/Livewire/LandingPage.php
+++ app/Http/Livewire/LandingPage.php
@@ -1,19 +1,25 @@
 <?php
-
 namespace App\Http\Livewire;
-
+use App\Models\Subscriber;
 use Livewire\Component;
 use Illuminate\Support\Facades\Log;
-
 class LandingPage extends Component
 {
     public $email;
 
+    protected $rules = [
+        'email' => 'required|email:filter|unique:subscribers,email',
+    ];
+
     public function subscribe()
     {
-        Log::debug($this->email);
-    }
+        $this->validate();
 
+        $subscriber = Subscriber::create([
+            'email' => $this->email,
+        ]);
+        $this->reset('email');
+    }
     public function render()
     {
         return view('livewire.landing-page');
