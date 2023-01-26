f0b828d264b565ffd19a4995a89b19a89caa5597
“mahasedra” <mahasedra01@gmail.com>
2023-01-26 19:17:16 +0300

    feat: Improve Functionalities and User Experience Using Livewire

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,7 +1,7 @@
 @props(['trigger'])
 
 <div class="flex fixed top-0 bg-opacity-60 items-center w-full h-full" x-show="{{ $trigger }}"
-    x-on:click.self="{{ $trigger }} = false" x-on:keydown.escape.window="{{ $trigger }} = false">
+    x-on:click.self="{{ $trigger }} = false" x-on:keydown.escape.window="{{ $trigger }} = false" x-cloak>
     <div {{ $attributes->merge(['class' => 'm-auto shadow-2xl rounded-xl p-8']) }}>
         {{ $slot }}
     </div>
--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -38,12 +38,17 @@
         </p>
         <form class="flex flex-col items-center p-24" wire:submit.prevent="subscribe">
             <x-input class="px-5 py-3 w-80 border border-blue-400" type="email" name="email"
-                placeholder="Email address" wire:model="email"></x-input>
+                placeholder="Email address" wire:model.defer="email"></x-input>
             <span class="text-gray-100 text-xs">
                 {{ $errors->has('email') ? $errors->first('email') : 'We will send you a confirmation email.' }}
             </span>
             <x-button class="px-5 py-3 mt-5 w-80 bg-blue-500 justify-center">
-                Get In
+                <span class="animate-spin" wire:loading wire:target="subscribe">
+                    &#9696;
+                </span>
+                <span wire:loading.remove wire:target="subscribe">
+                    Get In
+                </span>
             </x-button>
         </form>
     </x-modal>
